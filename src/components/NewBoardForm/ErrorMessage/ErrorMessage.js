2d67fb83ccbd3875b604274b07cb325dd62f3118
asmi902 <59359948+asmi902@users.noreply.github.com>
2024-07-11 16:59:09 +0530

    Add files via upload

---
+++ src/components/NewBoardForm/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+import { ERRORS } from './../../../services/Solver/constants';
+import AlertTriangle from '../../Icons/AlertTriangle/AlertTriangle';
+
+const errorMessage = (props) => {
+	switch (props.error) {
+		case ERRORS.INVALID_LENGTH: {
+			return (
+				<div className={props.classes}>
+					<AlertTriangle classes="App-icon abort-board" />
+					<h5 className="App-status-message">
+						The string length must be equal to 81
+					</h5>
+				</div>
+			);
+		}
+		case ERRORS.EMPTY_VALUE: {
+			return (
+				<div className={props.classes}>
+					<AlertTriangle classes="App-icon abort-board" />
+					<h4 className="App-status-abort-message">Enter a valid string</h4>
+				</div>
+			);
+		}
+		case ERRORS.INVALID_VALUE: {
+			return (
+				<div className={props.classes}>
+					<AlertTriangle classes="App-icon abort-board" />
+					<h4 className="App-status-abort-message">
+						Valid values must be . and numbers in the range 1-9
+					</h4>
+				</div>
+			);
+		}
+		default: {
+			return <div className={props.classes}></div>;
+		}
+	}
+};
+
+export default errorMessage;
