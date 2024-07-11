49c13997d9cf6630f43b8abbaf43f1261cd98105
asmi902 <59359948+asmi902@users.noreply.github.com>
2024-07-11 17:06:48 +0530

    Add files via upload

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,1 +1,144 @@
+import React, { useEffect, useReducer } from 'react';
+import Board from '../Board/Board';
+import Button from '../Button/Button';
+import Circle from '../Circle/Circle';
+import StepsLog from '../StepsLog/StepLogs';
+import Modal from '../../components/Modal/Modal';
+import Header from '../Header/header';
+import NewBoardForm from '../../components/NewBoardForm/NewBoardForm';
+import StatusMessage from '../StatusMessage/StatusMessage';
+import { ACTIONS } from '../../services/Solver/constants';
+import Solver from '../../services/Solver/solver';
+import { appReducer, initialState } from './reducer';
+import './style.css';
 
+const app = (props) => {
+	const [state, dispatch] = useReducer(appReducer, initialState);
+
+	const {
+		isSolving,
+		initialBoardParsed,
+		initialBoardState,
+		initialBoardStatus,
+		solveBoardState,
+		solveBoardStatus,
+		solveBoardAbort,
+		solutionSteps,
+		newBoardString,
+		timerSolveBoard,
+		timeElapsed,
+		openModal,
+		modalError,
+	} = state;
+
+	useEffect(() => {
+		const raw = localStorage.getItem('data');
+		dispatch({ type: ACTIONS.RELOAD, payload: JSON.parse(raw) });
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('data', JSON.stringify(state));
+	}, [state]);
+
+	const solverHandler = () => {
+		dispatch({ type: ACTIONS.SOLVE });
+
+		Solver(initialBoardParsed).then((result) => {
+			dispatch({ type: ACTIONS.SUCCESS, result: result });
+		});
+	};
+
+	const onLearnClick = () => {
+		console.log('onLearnClick  clicked!');
+	};
+
+	return (
+		<div className="App">
+			<Header />
+
+			<div className="App-body App-container">
+				<div className="App-game-panel">
+					<Circle classes="circle center-me" label="1" />
+					<Button
+						classes="btn marginTop15"
+						click={() => dispatch({ type: ACTIONS.OPEN })}
+						label="Load New Board"
+					/>
+					<Board
+						classes="game-board"
+						board={initialBoardState}
+						name="initial"
+					/>
+					<div className="buttons-row">
+						<Button
+							classes="btn btn-small"
+							click={() => dispatch({ type: ACTIONS.DEFAULT })}
+							label="Use Default Board"
+						/>
+					</div>
+					<StatusMessage
+						classes="App-message-row"
+						status={initialBoardStatus}
+					/>
+				</div>
+
+				<div className="App-game-panel">
+					<Circle classes="circle center-me" label="2" />
+					<div>
+						<Button
+							classes="btn marginTop15"
+							isSpinning={isSolving}
+							click={solverHandler}
+							label="Solve"
+						/>
+					</div>
+
+					<Board classes="game-board" board={solveBoardState} name="solver" />
+					<div className="buttons-row">
+						<Button
+							classes="btn btn-small marginLeft10"
+							click={() => dispatch({ type: ACTIONS.CLEAR })}
+							label="Clear"
+						/>
+					</div>
+					<StatusMessage
+						classes="App-message-row"
+						status={timerSolveBoard}
+						timeElapsed={timeElapsed}
+					/>
+					<StatusMessage classes="App-message-row" status={solveBoardStatus} />
+					<StatusMessage classes="App-message-row" status={solveBoardAbort} />
+				</div>
+
+				<div className="App-game-panel">
+					<Circle classes="circle center-me" label="3" />
+					<div className="buttons-row">
+						<Button classes="btn" click={onLearnClick} label="Learn" />
+					</div>
+					<StepsLog classes="steps-log" stepsLog={solutionSteps} />
+				</div>
+			</div>
+			<Modal
+				show={openModal}
+				modalClosed={() => dispatch({ type: ACTIONS.CLOSE })}
+			>
+				<NewBoardForm
+					modalClosed={() => dispatch({ type: ACTIONS.CLOSE })}
+					changed={(e) =>
+						dispatch({
+							type: ACTIONS.SET,
+							field: 'newBoardString',
+							value: e.target.value,
+						})
+					}
+					click={() => dispatch({ type: ACTIONS.CHANGE })}
+					currentStringBoard={newBoardString}
+					error={modalError}
+					randomPuzzle={() => dispatch({ type: ACTIONS.RANDOM })}
+				/>
+			</Modal>
+		</div>
+	);
+};
+
+export default app;
---
+++ src/components/App/reducer.js
@@ -0,0 +1,152 @@
+import {
+	STATUS,
+	ACTIONS,
+	initialSudokuString,
+	emptySudokuString,
+} from '../../services/Solver/constants';
+import {
+	getBoardState,
+	getRandomPuzzle,
+	isSolved,
+	stringBoardValidation,
+} from '../../services/Solver/utils';
+import { parseGrid } from '../../services/Solver/solver';
+import { resetLog } from '../../services/Solver/logs';
+
+const reset = (state) => {
+	const emptyBoardParsed = parseGrid(emptySudokuString);
+	return {
+		...state,
+		initialBoardStatus: STATUS.UNKNOWN,
+		solveBoardStatus: STATUS.UNKNOWN,
+		solveBoardAbort: STATUS.UNKNOWN,
+		solutionSteps: resetLog('solutionSteps'),
+		solveBoardState: getBoardState(emptyBoardParsed),
+		openModal: false,
+		timerSolveBoard: STATUS.UNKNOWN,
+		timeElapsed: 0,
+		modalError: '',
+	};
+};
+
+const change = (state, newBoardString) => {
+	const newBoardParsed = parseGrid(newBoardString);
+	return {
+		...reset(state),
+		initialBoardStatus: isSolved(newBoardParsed) ? STATUS.SOLVE : '',
+		initialBoardParsed: newBoardParsed,
+		initialBoardState: getBoardState(newBoardParsed),
+		currentBoardString: newBoardString,
+		newBoardString: '',
+	};
+};
+
+export const appReducer = (state, action) => {
+	switch (action.type) {
+		case ACTIONS.RELOAD: {
+			return action.payload;
+		}
+		case ACTIONS.SET: {
+			return {
+				...state,
+				[action.field]: action.value,
+			};
+		}
+		case ACTIONS.OPEN: {
+			return {
+				...state,
+				modalError: '',
+				newBoardString: '',
+				openModal: true,
+			};
+		}
+		case ACTIONS.CLOSE: {
+			return {
+				...state,
+				openModal: false,
+				newBoardString: '',
+				modalError: '',
+			};
+		}
+		case ACTIONS.SOLVE: {
+			return {
+				...state,
+				isSolving: true,
+			};
+		}
+		case ACTIONS.SUCCESS: {
+			const initialBoardParsed = parseGrid(state.currentBoardString);
+			const result = action.result;
+			const resultBoardParsed = result.board;
+			return {
+				...state,
+				isSolving: false,
+				solveBoardStatus: result.status,
+				initialBoardStatus: STATUS.UNKNOWN,
+				solveBoardAbort: result.abort ? STATUS.ABORT : STATUS.UNKNOWN,
+				timerSolveBoard: result.abort ? STATUS.ABORT : STATUS.TIMER,
+				timeElapsed: result.timer.toFixed(2),
+				solveBoardState: getBoardState(resultBoardParsed),
+				solutionSteps: result.solutionSteps,
+				initialBoardParsed: initialBoardParsed,
+				initialBoardState: getBoardState(initialBoardParsed),
+			};
+		}
+		case ACTIONS.CHANGE: {
+			const isValidString = stringBoardValidation(state.newBoardString);
+			if (isValidString === true) {
+				return change(state, state.newBoardString);
+			} else {
+				const validationError = isValidString;
+				return {
+					...state,
+					'modalError': validationError,
+				};
+			}
+		}
+		case ACTIONS.RANDOM: {
+			const randomPuzzleString = getRandomPuzzle();
+			return change(state, randomPuzzleString);
+		}
+		case ACTIONS.CLEAR: {
+			const initialBoardParsed = parseGrid(state.currentBoardString);
+			return {
+				...reset(state),
+				initialBoardParsed: initialBoardParsed,
+				initialBoardState: getBoardState(initialBoardParsed),
+			};
+		}
+		case ACTIONS.DEFAULT: {
+			const initialBoardParsed = parseGrid(initialSudokuString);
+			return {
+				...reset(state),
+				initialBoardParsed: initialBoardParsed,
+				initialBoardState: getBoardState(initialBoardParsed),
+				currentBoardString: initialSudokuString,
+			};
+		}
+		default: {
+			throw new Error(`Unhandled type: ${action.type}`);
+		}
+	}
+};
+
+const initialBoardParsed = parseGrid(initialSudokuString);
+const emptyBoardParsed = parseGrid(emptySudokuString);
+
+export const initialState = {
+	currentBoardString: initialSudokuString,
+	initialBoardParsed: initialBoardParsed,
+	initialBoardState: getBoardState(initialBoardParsed),
+	initialBoardStatus: STATUS.UNKNOWN,
+	solveBoardState: getBoardState(emptyBoardParsed),
+	solveBoardStatus: STATUS.UNKNOWN,
+	solveBoardAbort: false,
+	isSolving: false,
+	solutionSteps: [],
+	newBoardString: '',
+	timerSolveBoard: STATUS.UNKNOWN,
+	timeElapsed: 0,
+	openModal: false,
+	modalError: '',
+};
